2879c495003f5b6231d0b5ede035134a5b99f042
Mayra Alzate <nadja.minni@gmail.com>
2025-08-19 18:22:19 -0500

    finalización main

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -8,17 +8,42 @@ public class Main {
         Profesor prof3 = new Profesor("Jaime Florez", "67890430", 2000);
         Profesor prof4 = new Profesor("Carmen Soto", "78934023", 0, 0, false);
 
-
+        System.out.println("============================= Info profesores==================================");
 
         prof1.mostrarInformacion();
         prof2.mostrarInformacion();
         prof3.mostrarInformacion();
         prof4.mostrarInformacion();
-    
 
-        prof2.calcularSalarioAnual();
-        prof2.calcularSalarioAnual(4);
-        prof2.calcularSalarioAnual(5000);
+        System.out.println(
+                "================================== Salario Anual y sus respectivos bonos=======================");
+
+        System.out.println(prof3.calcularSalarioAnual());
+        System.out.println(prof3.calcularSalarioAnual(4));
+        System.out.println(prof3.calcularSalarioAnual(5));
+
+        System.out.println("==================== Carga horaria==========================");
+
+        System.out.println(prof2.evaluarCargaHoraria());
+        System.out.println(prof2.evaluarCargaHoraria(20));
+
+        System.out.println("=============Horas anuales======================");
+
+        System.out.println(prof1.calcularHorasAnuales());
+
+
+        Curso curso1=new Curso("Matemáticas", prof4);
+
+
+        System.out.println("====================Info curso=================");
+
+        curso1.asignarProfesor(prof4);
+
+        System.out.println("==================Asignación de horas semanales====================");
+
+        prof1.asignarHorasSemanales(35);
+
+
 
     }
 }
